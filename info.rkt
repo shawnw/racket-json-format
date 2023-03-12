81f011ae2a8b13d8adcd28b23120b61cbbded3d0
Shawn Wagner <shawnw.mobile@gmail.com>
2023-03-12 13:23:27 -0700

    And the CI satisfaction dance begins...

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" ("racket" #:version "8.4")))
-(define build-deps '("scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 ;(define scribblings '(("json/format/scribblings/json-format.scrbl" ())))
 (define pkg-desc "JSON pretty printers")
 (define version "0.0")
--- json/info.rkt
+++ json/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "json-format")
 (define deps '("base" ("racket" #:version "8.4")))
-(define build-deps '("scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("format/scribblings/json-format.scrbl" ())))
 
